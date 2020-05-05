4c7b52e50ee542bad178326d6354399c0f0ad4ba
急須 <feedmewithspam@web.de>
2020-05-05 14:30:32 +0200

    Add unit test for countryIsNotOnBlackList module

---
+++ src/steps/countryIsNotOnBlackList.test.ts
@@ -0,0 +1,39 @@
+import countryIsNotOnBlackList from "./countryIsNotOnBlackList";
+import { factory } from "factoree";
+import {
+  BandInProcessingStep,
+  FilteredOutEntry,
+  WithValidatedCountryCode
+} from "../types/Band";
+import { left, right } from "fp-ts/lib/Either";
+
+const createWithValidatedCountryCode = factory<WithValidatedCountryCode>({
+  maEntry: undefined,
+  countryCode: undefined
+});
+const createFilteredOutEntry = factory<FilteredOutEntry>({
+  maEntry: undefined,
+  reason: undefined
+});
+
+describe("countryIsNotOnBlackList", () => {
+  it("should wrap the entry in a right if the country of the band is not on the black list", () => {
+    const bandFromBrazil = createWithValidatedCountryCode({
+      countryCode: "BR"
+    });
+    const result = countryIsNotOnBlackList(bandFromBrazil);
+    expect<BandInProcessingStep<WithValidatedCountryCode>>(result).toEqual(
+      right(createWithValidatedCountryCode({ countryCode: "BR" }))
+    );
+  });
+
+  it("should wrap the entry in a left if the country of the band is on the black list", () => {
+    const bandFromHongKong = createWithValidatedCountryCode({
+      countryCode: "HK"
+    });
+    const result = countryIsNotOnBlackList(bandFromHongKong);
+    expect<BandInProcessingStep<WithValidatedCountryCode>>(result).toEqual(
+      left(createFilteredOutEntry({ reason: "Country is too small" }))
+    );
+  });
+});
